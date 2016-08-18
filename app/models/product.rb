a7991d7e9a269c2a3ec054a25a2a7fbb5b0c9607
nqdien <nqdien@mytrax.co.jp>
2016-08-18 13:41:40 +0700

    Depot B

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence:true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01, message:"must be greater than or equal to 0.01 - change message"}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
