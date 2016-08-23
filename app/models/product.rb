06066a02b60506c1d6817867414ab5edf27a0abf
nqdien <nqdien@mytrax.co.jp>
2016-08-23 16:57:16 +0700

    Depot D

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence:true
   validates :price, numericality: {greater_than_or_equal_to: 0.01, message:"must be greater than or equal to 0.01 - change message"}
   validates :title, uniqueness: true
@@ -10,4 +13,13 @@ class Product < ActiveRecord::Base
   def self.latest
     Product.order(:updated_at).last
   end
+
+  private
+
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
